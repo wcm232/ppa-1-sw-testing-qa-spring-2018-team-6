f0f405f64e1cc089fc5c6d7811a4ac295ae54696
Evan Dixon <evanldixon@live.com>
2018-02-16 16:02:32 -0600

    Implement remaining functionality

--- project/emailVerifier.py
+++ project/emailVerifier.py
@@ -1,4 +1,21 @@
 import re
 
 def verifyEmail(strEmailAddress):
-    return re.match("^([a-z])*\@([a-z]|\.)*$", strEmailAddress) is not None
+    if strEmailAddress is None:
+        return False
+
+    if type(strEmailAddress) is not str:
+        return False
+
+    if ".." in strEmailAddress:
+        return False
+
+    if strEmailAddress.startswith("."):
+        return False
+
+    # Make sure the string left of the @ doesn't end with a period
+    # The regex further on will make sure there's only 1 @
+    if ".@" in strEmailAddress:        
+        return False
+
+    return re.match("""^[a-z\!\$\%\*\+\-\=\?\^\_\{\|\}\~]([a-z0-9\!\$\%\*\+\-\=\?\^\_\{\|\}\~]|\.)*\@([a-z0-9]|\.)+$""", strEmailAddress) is not None
