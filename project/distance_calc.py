a83f3140b9cea6c2cd0b523b9ace4d8a5c2cbd17
Chris-Michael-Allen <32909754+Chris-Michael-Allen@users.noreply.github.com>
2018-02-17 16:56:39 -0600

    Add files via upload
    Implemented requested changes

--- project/distance_calc.py
+++ project/distance_calc.py
@@ -1,11 +1,11 @@
 import math
 
-def calc(a,b,c,d):
+def calculateDistance(coord1_x,coord1_y,coord2_x,coord2_y):
 
-      for argument in (a, b, c, d):
+      for argument in (coord1_x, coord1_y, coord2_x, coord2_y):
             if type(argument) != int and type(argument) != float:
                   return False
             
-      distance = math.sqrt((a-c)**2+(b-d)**2)
-      return float("{0:.3f}".format(round(distance,3)))
+      distance = math.sqrt((coord1_x-coord2_x)**2+(coord1_y-coord2_y)**2)
+      return float((round(distance,3)))
 
