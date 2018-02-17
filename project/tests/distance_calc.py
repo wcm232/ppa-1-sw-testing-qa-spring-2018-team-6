b4534023cd85d78bb74c9cfe1b2e788577a5a144
Chris-Michael-Allen <32909754+Chris-Michael-Allen@users.noreply.github.com>
2018-02-17 12:57:30 -0600

    Delete distance_calc.py
    Placing functioning code in the proper location

--- project/tests/distance_calc.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def calc(a,b,c,d):
-      
-      if(type(a) == str):
-            raise TypeError
-      else:
-            distance = math.sqrt((a-c)**2+(b-d)**2)
-            return float("{0:.3f}".format(round(distance,3)))
