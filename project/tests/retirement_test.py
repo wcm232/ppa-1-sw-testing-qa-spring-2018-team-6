5964b6b917798a6a023cfe07da28551d3c7f30c5
jtidwell19 <jst238@msstate.edu>
2018-02-18 14:43:29 -0600

    Add files via upload

--- project/tests/retirement_test.py
+++ project/tests/retirement_test.py
@@ -15,6 +15,12 @@ class retirementTesting(unittest.TestCase):
     def test_age_out_of_range_above(self):
         self.assertFalse(retirement(101, 100000.0, 35.0, 50000))
 
+    def test_age_edge(self):
+        self.assertFalse(retirement(100, 100000.0, 35.0, 100000))
+
+    def test_age_out_of_range(self):
+        self.assertFalse(retirement(95, 100000.0, 1.0, 100000))
+
     def test_salary_int(self):
         self.assertFalse(retirement(30, 100000, 35.0, 50000))
 
@@ -30,6 +36,12 @@ class retirementTesting(unittest.TestCase):
     def test_saved_int(self):
         self.assertFalse(retirement(30, 100000.0, 35, 50000))
 
+    def test_saved_below(self):
+        self.assertFalse(retirement(30, 100000.0, -3.0, 50000))
+
+    def test_saved_above(self):
+        self.assertFalse(retirement(30, 100000.0, 101.0, 50000))
+
     def test_goal_string(self):
         self.assertFalse(retirement(30, 100000.0, 35.0, '50000'))
 
@@ -40,7 +52,10 @@ class retirementTesting(unittest.TestCase):
         self.assertTrue(retirement(30, 100000.0, 35.0, 50000))
 
     def test_goal_reach2(self):
-        self.assertTrue(retirement(22, 80000.0, 15.0, 1000000)) 
+        self.assertTrue(retirement(22, 80000.0, 15.0, 1000000))
+
+    def test_goal_below(self):
+        self.assertFalse(retirement(30, 100000.0, 35.0, -3))
 
 if __name__ == '__main__':
     unittest.main()
