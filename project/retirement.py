324f8bc7449edd99fbaa974595f409667e2b09a7
jtidwell19 <jst238@msstate.edu>
2018-02-18 12:08:08 -0600

    Add files via upload

--- project/retirement.py
+++ project/retirement.py
@@ -1,14 +1,40 @@
 import unittest
 
 def retirement(age, salary, saved, goal):
+    if type(age) is not int:
+        print("Your age must be a valid intger type.")
+        return False
+    if type(salary) is not float:
+        print("Your salary must be a valid integer type.")
+        return False
+    if type(saved) is not float:
+        print("The percentage you wish to save every year must be a valid integer.")
+        return False
+    if type(goal) is not int:
+        print("Your goal must be a valid integer type.")
+        return False
+    if age < 16:
+        print("Invalid user age.")
+        return False
+    if age >= 100:
+        print("Invalid user age.")
+        return False
+    if salary < 0:
+        print("Invalid salary")
+        return False
     saved = saved / 100
     savedAmount = salary * saved
+    employerMatch = savedAmount * 0.35
     total = savedAmount
-    if total >= goal:
-        return True;
     while (100-age) > 0:
+        total1 = total + (total * 0.35)
+        if total1 >= goal:
+            print("Savings goal is met at the age of: %d" % age)
+            return True
+        total += savedAmount
         age += 1
-    return False;
+    print("Your savings goal will not be met at the rate you are saving.")
+    return False
 
 if __name__ == '__main__':
     unittest.main()
@@ -16,9 +42,9 @@ if __name__ == '__main__':
 #The main function of the program
 #print("Welcome to retirement plan calculator!")
 #print("Please enter you current age, annual salary, how much you save per year, and your desired retirement savings goal.")
-#age = input("Current Age: ")
-#salary = input("Annual Salary: ")
+#age = int(input("Current Age: "))
+#salary = float(input("Annual Salary: "))
 #saved = float(input("Save Percentage(enter without a percent sign): "))
-#goal = input("Retirement Goal: ")
+#goal = int(input("Retirement Goal: "))
 #retirement(age, salary, saved, goal)
 #print("Thank you for your service!")
