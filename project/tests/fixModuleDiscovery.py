9b03a98835c67e486364d1ff41be49d38a589c19
jtidwell19 <jst238@msstate.edu>
2018-02-18 12:10:55 -0600

    Add files via upload

---
+++ project/tests/fixModuleDiscovery.py
@@ -0,0 +1,3 @@
+import sys
+sys.path.append("..") #In case we're running in /project/tests
+sys.path.append("project") #In case we're running in /
--- project/tests/retirement_test.py
+++ project/tests/retirement_test.py
@@ -1,12 +1,31 @@
 import unittest
 from retirement import retirement
+import fixModuleDiscovery
 
 class retirementTesting(unittest.TestCase):
-    def test_age_counter(self):
-        self.assertFalse(retirement(99, 10, 10, 10))
+    def test_age_string(self):
+        self.assertFalse(retirement('30', 100000.0, 35.0, 50000))
 
-    def test_goal_reach(self):
-        self.assertTrue(retirement(10, 100000, 10, 5000))
+    def test_age_out_of_range_below(self):
+        self.assertFalse(retirement(10, 100000.0,35.0,50000))
+
+    def test_age_out_of_range_above(self):
+        self.assertFalse(retirement(101, 100000.0, 35.0, 50000))
+
+    def test_salary_int(self):
+        self.assertFalse(retirement(30, 100000, 35.0, 50000))
+
+    def test_salary_string(self):
+        self.assertFalse(retirement(30, '100000.0', 35.0, 50000))
+
+    def test_salary_below(self):
+        self.assertFalse(retirement(30, -3, 35.0, 50000))
+
+    def test_goal_reach1(self):
+        self.assertTrue(retirement(30, 100000.0, 35.0, 50000))
+
+    def test_goal_reach2(self):
+        self.assertTrue(retirement(22, 80000.0, 15.0, 1000000)) 
 
 if __name__ == '__main__':
     unittest.main()
