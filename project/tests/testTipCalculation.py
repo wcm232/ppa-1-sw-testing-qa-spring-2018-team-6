576f6eb6059c8061b701571018a1243ff642590b
Evan Dixon <evanldixon@live.com>
2018-02-18 22:19:35 -0600

    Implement tip splitting functionality

---
+++ project/tests/testTipCalculation.py
@@ -0,0 +1,28 @@
+import fixModuleDiscovery
+import unittest
+import tip_calc
+
+class TestTip(unittest.TestCase):
+      def test_require_number_arg1(self):
+            for badInput in ["10", True, TestTip()]:
+                  with self.assertRaises(TypeError):                        
+                        tip_calc.calculateTip(badInput, 1)
+      def test_require_number_arg2(self):
+            for badInput in ["10", True, TestTip()]:
+                  with self.assertRaises(TypeError):                        
+                        tip_calc.calculateTip(badInput, 1)
+      def test_fail_zero_way_split(self):
+            with self.assertRaises(ValueError):                        
+                  tip_calc.calculateTip(1, 0)
+      def test_fail_negative_way_split(self):
+            with self.assertRaises(ValueError):                        
+                  tip_calc.calculateTip(1, -1)
+      def test_one_way_split(self):
+            self.assertEqual([1.15], tip_calc.calculateTip(1, 1))
+            self.assertEqual([5.75], tip_calc.calculateTip(5, 1))
+      def test_two_way_even_split(self):
+            self.assertEqual([0.69, 0.69], tip_calc.calculateTip(1.2, 2))
+            self.assertEqual([2.99, 2.99], tip_calc.calculateTip(5.2, 2))
+      def test_three_way_uneven_split(self):
+            self.assertEqual([0.30, 0.31, 0.31], tip_calc.calculateTip(0.8, 3))
+            self.assertEqual([5.82, 5.83, 5.83], tip_calc.calculateTip(15.2, 3))
--- project/tip_calc.py
+++ project/tip_calc.py
@@ -1,5 +1,37 @@
-def tip_calc(total_bill, num_people):
-    total_tip = total_bill * (.15)
-    total_bill += total_tip
+import math
+
+def calculateTip(sub_total, num_people):
+    if type(sub_total) is not int and type(sub_total) is not float:
+        raise TypeError()
+
+    if type(num_people) is not int:
+        raise TypeError()
+
+    if num_people < 1:
+        raise ValueError()    
+
+    total_bill = round(sub_total * 1.15, 2)
     per_person = total_bill / num_people
-    return per_person
+
+    dues = []
+    per_person_rounded = round(per_person, 2)   
+    unassigned_bill = total_bill
+    
+    for p in range(0, num_people):
+        unassigned_bill -= per_person_rounded
+        dues.append(per_person_rounded)
+    
+    remainderIncrement = 0
+    unassigned_bill = round(unassigned_bill, 2)
+    if unassigned_bill > 0:
+        remainderIncrement = 0.01
+    elif unassigned_bill < 0:
+        remainderIncrement = -0.01
+
+    if remainderIncrement != 0:        
+        for p in range(0, num_people):
+            unassigned_bill -= remainderIncrement
+            dues[p] = round(dues[p] + remainderIncrement, 2)            
+            if round(unassigned_bill, 2) == 0:
+                break
+    return dues
